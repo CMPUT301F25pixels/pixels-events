89b256a38ac5c123c56fd4a6dfa2cadf84a12a46
Krupal Shah <113124318+Krupal-Shah@users.noreply.github.com>
2025-09-25 17:50:30 -0600

    Add color property to Shape class

--- app/src/main/java/com/example/pixels/Shape.java
+++ app/src/main/java/com/example/pixels/Shape.java
@@ -3,6 +3,7 @@ package com.example.pixels;
 public abstract class Shape {
     protected int x;
     protected int y;
+    String color = "phthalo green";
 
     public Shape(int x, int y) {
         this.x = x;
