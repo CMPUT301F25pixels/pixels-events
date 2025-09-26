57b979576fa5d742b3adc5ed50a47158f9597674
Qendrim Beka <beka.qendrim1@gmail.com>
2025-09-26 13:20:04 -0600

    updated shape class with color

--- app/src/main/java/com/example/pixels/Shape.java
+++ app/src/main/java/com/example/pixels/Shape.java
@@ -3,7 +3,7 @@ package com.example.pixels;
 public abstract class Shape {
     protected int x;
     protected int y;
-    String color = "phthalo green";
+    String color = "green";
 
     public Shape(int x, int y) {
         this.x = x;
